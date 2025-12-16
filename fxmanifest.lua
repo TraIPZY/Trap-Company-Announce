338dda9e02307a657e01cce55e857b5b0aaa8212
Trap <148614470+TraIPZY@users.noreply.github.com>
2025-12-15 23:59:43 -0500

    Create fxmanifest.lua

---
+++ fxmanifest.lua
@@ -0,0 +1,14 @@
+fx_version 'cerulean'
+game 'gta5'
+
+author 'TRAPZY'
+description 'ESX Job Announcement UI'
+
+ui_page 'html/ui.html'
+
+files {
+    'html/ui.html'
+}
+
+client_script 'client.lua'
+server_script 'server.lua'
